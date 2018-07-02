1697495630949a43cb3eef019809fb7334548d58
Karel Nguyen <nguyenkar@seznam.cz>
2018-07-02 11:27:54 +0200

    fix some bugs + change typography

--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -3,19 +3,17 @@ import ProductsService from '../services/api/products'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import styled from 'styled-components'
-import AddIcon from '@material-ui/icons/Add';
-import Button from '@material-ui/core/Button';
-import Tooltip from '@material-ui/core/Tooltip';
+import AddIcon from '@material-ui/icons/Add'
+import Button from '@material-ui/core/Button'
+import Tooltip from '@material-ui/core/Tooltip'
+import Typography from '@material-ui/core/Typography'
+
 
 const InnerWrapper = styled.div`
   padding: 20px;
 `
 // TODO: dodelat, ted je to jak hovno
 class AddProductPage extends React.Component {
-  state = {
-    products: []
-  }
-
   addProduct () {
     return ProductsService
       .add()
@@ -24,9 +22,14 @@ class AddProductPage extends React.Component {
   render () {
     return (
       <div>
-        <h1>Přidat produkt</h1>
+        <Typography variant="display3" gutterBottom>
+          Přidat produkt
+        </Typography>
         <Paper>
           <InnerWrapper>
+            <Typography variant="body2" gutterBottom>
+              Zadejte hodnoty
+            </Typography>
             <TextField
               id="id"
               label="id"
